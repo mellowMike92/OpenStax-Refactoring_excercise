5b2064cf92dc90281fad6047772bc80020a1258a
Michael Issa <michael.f.issa@gmail.com>
2019-12-07 11:30:57 -0600

    Commented out main block in the trivia_game.py file

--- src/Trivia/trivia_game.py
+++ src/Trivia/trivia_game.py
@@ -21,23 +21,29 @@ class Game(ProcessAnswer):
         Roll.roll_logic(self, roll_number)
 
 
-if __name__ == '__main__':
+""" 
 
-    not_a_winner = False
+#  The main block has been copied into __main__.py, but the code will be left here for future testing/development.
 
-    game = Game()
+ if __name__ == '__main__':
 
-    game.add('Chet')
-    game.add('Pat')
-    game.add('Sue')
+     not_a_winner = False
 
-    while True:
-        game.roll(randrange(5) + 1)
+     game = Game()
 
-        if randrange(9) == 7:
-            not_a_winner = game.wrong_answer()
-        else:
-            not_a_winner = game.was_correctly_answered()
+     game.add('Chet')
+     game.add('Pat')
+     game.add('Sue')
 
-        if not not_a_winner:
-            break
+     while True:
+         game.roll(randrange(5) + 1)
+
+         if randrange(9) == 7:
+             not_a_winner = game.wrong_answer()
+         else:
+             not_a_winner = game.was_correctly_answered()
+
+         if not not_a_winner:
+             break
+             
+"""
